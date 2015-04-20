73659876f499f3b3da8b11060bcdf567264a0190
Nick <Nick@BatComputer>
2015-04-20 10:54:46 -0500

    Create and finish TempConverter

---
+++ src/temperatureconverter/StartUp.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package temperatureconverter;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+/**
+ *
+ * @author Nick
+ */
+public class StartUp {
+
+    /**
+     * @param args the command line arguments
+     * @throws javax.swing.UnsupportedLookAndFeelException
+     */
+    public static void main(String[] args) throws UnsupportedLookAndFeelException {
+        
+        UIManager.setLookAndFeel(UIManager.getLookAndFeel());
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MainWindow().setVisible(true);
+            }
+        });
+    }
+    
+}
